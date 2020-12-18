bd9baf31457524fda79c151cbc73ead54e3996a9
Gokul A Krishnan <gokulakrishnanmay29@gmail.com>
2020-12-18 17:00:53 +0530

    Auto commit

--- bench_test_service/test/bench_test_service_test.dart
+++ bench_test_service/test/bench_test_service_test.dart
@@ -3,11 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:bench_test_service/bench_test_service.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
-  });
+  RegisterResponse registerResponse = User().register(RegisterRequest(
+      'Balaji', 'balaji89237@gmail.com', '12345678', '12345678'));
+  print(registerResponse.toJson());
 }
